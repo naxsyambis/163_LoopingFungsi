473b6e74a4b3050d26f9ae108604c1597ed37bb1
naxsyambis <naxsyambis@gmail.com>
2024-03-19 11:35:00 +0000

    prosedur dan fungsi dalam menghitung luas persegi

---
+++ Procedur dan Fungsi/Procedur dan Fungsi.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void inputdata() {
+
+	cout << " Masukkan Nilai Panjang : ";
+	cin >> panjang;
+	cout << " Masukkan Nilai Lebar : ";
+	cin >> lebar;
+}
+int hitungluas() {
+	return panjang * lebar;
+}
+void bebas() {
+	cout << "Luasnya adalah :" << hitungluas() << endl;
+}
+
+int main() {
+	inputdata();
+	bebas();
+}
+   
+
+
